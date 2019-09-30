e759fc3b4c9b59891a688d79fadf30c3ae36ec50
Sahil Silare <sahilsilare@gmail.com>
2019-09-30 15:16:25 +0530

    Changed button text

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
             <form onSubmit = {getSearch} className="search-form">
         
                 <input className = "search-bar" type="text" value ={search} onChange={updateSearch}/>
-                <button className = "search-button" type="submit">Search</button>
+                <button className = "search-button" type="submit">Submit </button>
             </form>
             <div className="recipes">
             {recipes.map(recipe => (
