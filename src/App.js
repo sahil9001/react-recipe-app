af0d0ff2d7be8ab6530a9b67bc90be5f049a67d7
Harshit Jain <harshit_jain@intuit.com>
2019-10-03 19:33:17 +0530

    Fixed key issue

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{useEffect,useState} from 'react';
 import Recipe from "./Recipe";
 import { Button } from 'react-bootstrap';
+import uuid from 'uuid';
 
 const App = () => {
   
@@ -19,7 +20,7 @@ const App = () => {
     const getRecipes = async () =>{
         const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${api_id}&app_key=${api_key}`);
         const data = await response.json();
-        setRecipes(data.hits);
+        setRecipes([...data.hits]);
 
         
     };
@@ -44,7 +45,7 @@ const App = () => {
             <div className="recipes">
             {recipes.map(recipe => (
                 <Recipe 
-                key = {recipe.recipe.label} 
+                key = {uuid.v4()} // Generates a new random key everytime
                 title = {recipe.recipe.label} 
                 calories={recipe.recipe.calories} 
                 image = {recipe.recipe.image}
