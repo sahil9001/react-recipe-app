9c268f4d850fe3a45d8bfaa8cffd3601afa59aeb
Tarotato <isabelz123@gmail.com>
2019-10-03 21:38:48 +1300

    Add UI improvements to layout

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,14 +3,17 @@ import style from './recipe.module.css'
 const Recipe= ({title,calories,image,ingredients}) => {
     return(
         <div className={style.recipe}> 
+            <img className={style.image} src={image} alt=""/>
             <h1>{title}</h1>
             <p><b>Calories:</b> {Math.round(calories)} cal</p>
-            <img className={style.image} src={image} alt=""/>
+            <div className={style.list}>
             <ul className={style.ul}>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
                 ))}
             </ul>
+
+            </div>
             <br></br>
         </div>
     );
